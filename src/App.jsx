1d741b2dc3c9787419208c470b3a6fabfa8c11c3
selftech <selftechacademy@gmail.com>
2023-04-29 15:10:55 -0500

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import HeaderText from "./components/headerText/HeaderText.jsx";
+import "./App.css";
+
+function App() {
+  const schoolName = "Geek Network";
+  let age = 18;
+
+  return (
+    <>
+      <HeaderText />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/headerText/HeaderText.jsx
@@ -0,0 +1,12 @@
+import "./headerText.style.css";
+
+const HeaderText = () => {
+  return (
+    <>
+      <h1>I am header Text</h1>
+      <h2>I am the second header</h2>
+    </>
+  );
+};
+
+export default HeaderText;
