65f0085399a258adc364f4c55444beb8cc0b3d15
selftech <selftechacademy@gmail.com>
2023-05-01 18:58:13 -0500

    removed extra code

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,6 @@ import HeaderText from "./components/headerText/HeaderText.jsx";
 import "./App.css";
 
 function App() {
-  const schoolName = "Geek Network";
-  let age = 18;
-
   return (
     <>
       <HeaderText />
